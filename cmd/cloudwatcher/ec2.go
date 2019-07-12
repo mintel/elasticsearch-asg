5f0a12e2ba656318c1a9da2de6c84d45889cf4bd
Jaye Doepke <jdoepke@mintel.com>
2019-07-12 16:58:45 -0500

    Add debug logging to GetInstanceVCPUCount

--- cmd/cloudwatcher/ec2.go
+++ cmd/cloudwatcher/ec2.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 	cache "github.com/patrickmn/go-cache"
+	"go.uber.org/zap"
 )
 
 // Cache EC2 instance ID => int count of vcpu cores.
@@ -14,18 +15,23 @@ var vcpuCache = cache.New(5*time.Minute, 10*time.Minute)
 
 // GetInstanceVCPUCount gets the count of vCPUs for each EC2 instance in a list of instance IDs.
 func GetInstanceVCPUCount(ec2Svc ec2iface.EC2API, IDs []string) (map[string]int, error) {
+	logger := zap.L()
 	out := make(map[string]int, len(IDs))
-	toDesc := make([]*string, 0, len(IDs))
+	toDesc := make([]string, 0, len(IDs))
 	for _, id := range IDs {
-		if countPtr, ok := vcpuCache.Get(id); ok {
-			out[id] = countPtr.(int)
+		if v, ok := vcpuCache.Get(id); ok {
+			count := v.(int)
+			logger.Debug("GetInstanceVCPUCount cache hit", zap.String("instance_id", id), zap.Int("count", count))
+			out[id] = count
 		} else {
-			toDesc = append(toDesc, aws.String(id))
+			logger.Debug("GetInstanceVCPUCount cache miss", zap.String("instance_id", id))
+			toDesc = append(toDesc, id)
 		}
 	}
 	if len(toDesc) > 0 {
+		logger.Debug("DescribeInstances", zap.Strings("instance_ids", toDesc))
 		resp, err := ec2Svc.DescribeInstances(&ec2.DescribeInstancesInput{
-			InstanceIds: toDesc,
+			InstanceIds: aws.StringSlice(toDesc),
 		})
 		if err != nil {
 			return nil, err
@@ -33,9 +39,10 @@ func GetInstanceVCPUCount(ec2Svc ec2iface.EC2API, IDs []string) (map[string]int,
 		for _, r := range resp.Reservations {
 			for _, i := range r.Instances {
 				instanceID := *i.InstanceId
-				vcpuCount := int(*i.CpuOptions.CoreCount * *i.CpuOptions.ThreadsPerCore)
-				out[instanceID] = vcpuCount
-				vcpuCache.SetDefault(instanceID, vcpuCount)
+				count := int(*i.CpuOptions.CoreCount * *i.CpuOptions.ThreadsPerCore)
+				out[instanceID] = count
+				vcpuCache.SetDefault(instanceID, count)
+				logger.Debug("got instance vcpu count", zap.String("instance_id", instanceID), zap.Int("count", count))
 			}
 		}
 	}
