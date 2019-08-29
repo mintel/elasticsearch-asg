b1620abf7fc8abba1ba34ec547f382b17a1321e1
Jaye Doepke <jdoepke@mintel.com>
2019-08-28 21:17:14 -0500

    pkg/lifecycle: instrument

--- pkg/lifecycle/lifecycle.go
+++ pkg/lifecycle/lifecycle.go
@@ -11,13 +11,19 @@ import (
 	"errors"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"go.uber.org/zap" // Logging
 
 	// AWS clients and stuff
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/autoscaling/autoscalingiface"
+
+	"github.com/mintel/elasticsearch-asg/metrics"
 )
+
+const subsystem = "lifecycle"
 
 var (
 	// How much time to for checks and communication with AWS will take.
@@ -39,6 +45,76 @@ var (
 
 	// ErrExpired is returned when a lifecycle event should be expried according to its timeout and start timestamp.
 	ErrExpired = errors.New("lifecycle event has expired")
+)
+
+var (
+	// KeepAliveDuration is a Prometheus metric for the duration of the KeepAlive() function.
+	KeepAliveDuration = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: metrics.Namespace,
+		Subsystem: subsystem,
+		Name:      "keepalive_duration_seconds",
+		Help:      "Duration keeping ASG lifecycle hooks alive.",
+		Buckets:   prometheus.DefBuckets,
+	})
+	// KeepAliveErrors is a Prometheus metric that counts errors from the KeepAlive() function.
+	KeepAliveErrors = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: metrics.Namespace,
+		Subsystem: subsystem,
+		Name:      "keepalive_errors_total",
+		Help:      "Count of errors while keep ASG lifecycle hooks alive.",
+	})
+
+	// KeepAliveCheckDuration is a Prometheus metric for the duration of the KeepAlive() check functions.
+	KeepAliveCheckDuration = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: metrics.Namespace,
+		Subsystem: subsystem,
+		Name:      "keepalive_check_duration_seconds",
+		Help:      "Duration running ASG lifecycle hook keep-alive checks.",
+		Buckets:   prometheus.DefBuckets,
+	})
+	// KeepAliveCheckErrors is a Prometheus metric that counts errors from the KeepAlive() check functions.
+	KeepAliveCheckErrors = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: metrics.Namespace,
+		Subsystem: subsystem,
+		Name:      "keepalive_check_failed_total",
+		Help:      "Count of errors running ASG lifecycle hook keep-alive checks.",
+	}, []string{"error"})
+
+	// KeepAliveHeartbeatDuration is a Prometheus metric for the duration while recording
+	// AWS Autoscaling Group Lifecycle Hook heartbeats in the KeepAlive() function.
+	KeepAliveHeartbeatDuration = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: metrics.Namespace,
+		Subsystem: subsystem,
+		Name:      "keepalive_heartbeat_duration_seconds",
+		Help:      "Duration recording ASG lifecycle hook heartbeats.",
+		Buckets:   prometheus.DefBuckets,
+	})
+	// KeepAliveHeartbeatErrors is a Prometheus metric that counts errors while recording
+	// AWS Autoscaling Group Lifecycle Hook heartbeats in the KeepAlive() function.
+	KeepAliveHeartbeatErrors = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: metrics.Namespace,
+		Subsystem: subsystem,
+		Name:      "keepalive_heartbeat_errors_total",
+		Help:      "Count of errors recording ASG lifecycle hook heartbeats.",
+	})
+
+	// DescLifecycleHookDuration is a Prometheus metric for the duration while describing
+	// AWS Autoscaling Group Lifecycle Hooks.
+	DescLifecycleHookDuration = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: metrics.Namespace,
+		Subsystem: subsystem,
+		Name:      "desc_hook_duration_seconds",
+		Help:      "Duration describing ASG lifecycle hooks.",
+		Buckets:   prometheus.DefBuckets,
+	})
+	// DescLifecycleHookErrors is a Prometheus metric that counts errors while describing
+	// AWS Autoscaling Group Lifecycle Hooks.
+	DescLifecycleHookErrors = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: metrics.Namespace,
+		Subsystem: subsystem,
+		Name:      "desc_hook_errors_total",
+		Help:      "Count of errors describing ASG lifecycle hooks.",
+	})
 )
 
 // Transition is an enum representing the possible AWS Autoscaling Group
@@ -111,16 +187,19 @@ func NewEventFromMsg(ctx context.Context, client autoscalingiface.AutoScalingAPI
 	if !(e.LifecycleTransition == TransitionTerminating || e.LifecycleTransition == TransitionLaunching) {
 		return nil, ErrUnknownTransition
 	}
+	timer := prometheus.NewTimer(DescLifecycleHookDuration)
 	resp, err := client.DescribeLifecycleHooksWithContext(ctx, &autoscaling.DescribeLifecycleHooksInput{
 		AutoScalingGroupName: aws.String(e.AutoScalingGroupName),
 		LifecycleHookNames:   []*string{aws.String(e.LifecycleHookName)},
 	})
+	timer.ObserveDuration()
 	if err != nil {
+		DescLifecycleHookErrors.Inc()
 		return nil, err
 	}
 	e.HeartbeatTimeout = time.Duration(*resp.LifecycleHooks[0].HeartbeatTimeout) * timeoutIncrement
 	e.GlobalHeartbeatTimeout = time.Duration(*resp.LifecycleHooks[0].GlobalTimeout) * timeoutIncrement
-	return e, ctx.Err()
+	return e, nil
 }
 
 // GlobalTimeout returns the time past which the lifecycle transition cannot be delayed.
@@ -145,6 +224,9 @@ func (e *Event) Timeout() time.Time {
 //
 // The condition is is only checked just before the lifecycle event is due to expire.
 func KeepAlive(ctx context.Context, client autoscalingiface.AutoScalingAPI, e *Event, c func(context.Context, *Event) (bool, error)) error {
+	timer := prometheus.NewTimer(KeepAliveDuration)
+	defer timer.ObserveDuration()
+
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -155,6 +237,7 @@ func KeepAlive(ctx context.Context, client autoscalingiface.AutoScalingAPI, e *E
 	d := time.Until(timeout) - commBufD
 	zap.L().Debug("initial d", zap.Time("timeout", timeout), zap.Duration("d", d))
 	if d <= 0 {
+		KeepAliveErrors.Inc()
 		return ErrExpired
 	}
 	startCheck := time.After(d)
@@ -162,6 +245,7 @@ func KeepAlive(ctx context.Context, client autoscalingiface.AutoScalingAPI, e *E
 		select {
 		case <-ctx.Done():
 			zap.L().Debug("context done")
+			KeepAliveErrors.Inc()
 			return ctx.Err()
 
 		case <-startCheck:
@@ -169,25 +253,30 @@ func KeepAlive(ctx context.Context, client autoscalingiface.AutoScalingAPI, e *E
 			startCheck = nil                // Disable startCheck case
 			stopCheck = make(chan error, 1) // Enable stopCheck case
 			go func() {
-				if ok, err := c(ctx, e); err != nil {
+				timer := prometheus.NewTimer(KeepAliveCheckDuration)
+				ok, err := c(ctx, e)
+				timer.ObserveDuration()
+				if err != nil {
 					zap.L().Debug("check errored", zap.Error(err))
-					stopCheck <- err
+					KeepAliveCheckErrors.WithLabelValues("yes").Inc()
+					stopCheck <- err // Check errored
 				} else if ok {
 					zap.L().Debug("check passed")
-					stopCheck <- nil
+					stopCheck <- nil // Check passed
 				} else {
 					zap.L().Debug("check failed")
-					close(stopCheck)
+					KeepAliveCheckErrors.WithLabelValues("no").Inc()
+					close(stopCheck) // Check failed
 				}
 			}()
 
 		case err, ok := <-stopCheck:
 			zap.L().Debug("case: stopCheck", zap.Bool("ok", ok), zap.Error(err))
-			if err != nil { // check errored
-				return err
-			} else if ok { // check returned true
-				return nil
-			} // else check returned false
+			if err != nil {
+				return err // check errored
+			} else if ok {
+				return nil // check passed
+			} // else check failed
 
 			stopCheck = nil // Disable stop check case
 
@@ -205,12 +294,15 @@ func KeepAlive(ctx context.Context, client autoscalingiface.AutoScalingAPI, e *E
 			stopHeartbeat = make(chan error, 1) // Enable stopHeartbeat case
 			go func() {
 				defer close(stopHeartbeat)
+				timer := prometheus.NewTimer(KeepAliveHeartbeatDuration)
 				_, err = client.RecordLifecycleActionHeartbeatWithContext(ctx, &autoscaling.RecordLifecycleActionHeartbeatInput{
 					AutoScalingGroupName: aws.String(e.AutoScalingGroupName),
 					LifecycleHookName:    aws.String(e.LifecycleHookName),
 					LifecycleActionToken: aws.String(e.LifecycleActionToken),
 				})
+				timer.ObserveDuration()
 				if err != nil {
+					KeepAliveHeartbeatErrors.Inc()
 					e.HeartbeatCount-- // Undo heartbeat increment
 				}
 				stopHeartbeat <- err
